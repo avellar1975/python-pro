5087c111137b955db7caf17c8d0ff79eae967555
avellar1975 <evandro.avellar@gmail.com>
2020-08-16 17:39:23 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,24 @@
 """Classe Pessoa."""
 
 
-class Pessoa():
+class Pessoa:
     """Classe que cria objeto pessoa."""
 
-    def __init__(self, nome):
+    def __init__(self, nome, idade=35, *filhos):
         """Recebe o parametro nome na criação do objeto."""
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
+        """Método cumprimentar."""
         return f'Olá! Sou o {self.nome}'
 
+
 if __name__ == '__main__':
-    evandro = Pessoa('Evandro')
+    yanni = Pessoa(nome='Yanni', idade=22)
+    evandro = Pessoa('Evandro', 44, yanni)
+    print(f'{evandro.nome} tem {evandro.idade} e seus filhos são:')
+    for filho in evandro.filhos:
+        print(filho.nome)
     print(evandro.cumprimentar())
